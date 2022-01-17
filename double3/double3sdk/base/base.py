92fe1ad8d035919c6787e2c5cfe5730278043117
cdh <imcdh6739@naver.com>
2022-01-17 17:42:32 +0900

    feat: add pole_set_target in base

--- double3/double3sdk/base/base.py
+++ double3/double3sdk/base/base.py
@@ -2,4 +2,15 @@ from double3sdk.double_api import _DoubleAPI
 
 
 class _Base:
-    pass
+    def pole_set_target(self, percent) -> None:
+        '''
+        base.pole.setTarget
+            percent
+                0-100 사이의 실수
+                pole의 높이를 (percent)%로 설정
+        '''
+        double_api = _DoubleAPI()
+        double_api.send_command(
+            'base.pole.setTarget',
+            {"percent": percent}
+        )
