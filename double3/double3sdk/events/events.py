9557b28e6262613eac9235af8c157b5f4fac2654
cdh <imcdh6739@naver.com>
2022-01-25 11:00:27 +0900

    feat: add subscribe, unsubscribe in events

--- double3/double3sdk/events/events.py
+++ double3/double3sdk/events/events.py
@@ -2,4 +2,26 @@ from double3sdk.double_api import _DoubleAPI
 
 
 class _Events:
-    pass
+    def subscribe(self, events: list) -> None:
+        '''
+        events.subscribe
+            events
+                [ "DRBase.status" ]
+        '''
+        double_api = _DoubleAPI()
+        double_api.send_command(
+            'events.subscribe',
+            {"events": events}
+        )
+
+    def unsubscribe(self, events: list) -> None:
+        '''
+        events.unsubscribe
+            events
+                [ "DRBase.status" ]
+        '''
+        double_api = _DoubleAPI()
+        double_api.send_command(
+            'events.unsubscribe',
+            {"events": events}
+        )
