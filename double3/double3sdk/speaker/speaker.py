8b7ac80bb2b30aa45ea5bebcd0dbd94ae25a1dae
cdh <imcdh6739@naver.com>
2022-01-25 10:36:13 +0900

    feat: add enable, disable, set_volume in speaker

--- double3/double3sdk/speaker/speaker.py
+++ double3/double3sdk/speaker/speaker.py
@@ -2,4 +2,31 @@ from double3sdk.double_api import _DoubleAPI
 
 
 class _Speaker:
-    pass
+    def enable(self) -> None:
+        '''
+        speaker.enable
+            스피커 실행
+        '''
+        double_api = _DoubleAPI()
+        double_api.send_command('speaker.enable')
+
+    def disable(self) -> None:
+        '''
+        speaker.disable
+            스피커 끄기
+        '''
+        double_api = _DoubleAPI()
+        double_api.send_command('speaker.disable')
+
+    def set_volume(self, percent: float) -> None:
+        '''
+        speaker.setVolume
+            percent
+                0-100 사이의 실수
+                스피커의 볼륨을 (percent)%로 설정
+        '''
+        double_api = _DoubleAPI()
+        double_api.send_command(
+            'speaker.setVolume',
+            {"percent": percent}
+        )
