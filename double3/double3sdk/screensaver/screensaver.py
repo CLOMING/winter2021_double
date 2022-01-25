e3f0299fa69a808b5c4bef0649833e603fc9a416
cdh <imcdh6739@naver.com>
2022-01-25 10:20:26 +0900

    feat: add nudge in screensaver

--- double3/double3sdk/screensaver/screensaver.py
+++ double3/double3sdk/screensaver/screensaver.py
@@ -2,4 +2,10 @@ from double3sdk.double_api import _DoubleAPI
 
 
 class _Screensaver:
-    pass
+    def nudge(self) -> None:
+        '''
+        screensaver_nudge
+            반응이 들어오면 화면을 킴
+        '''
+        double_api = _DoubleAPI()
+        double_api.send_command('screensaver.nudge')
