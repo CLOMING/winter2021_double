f5447c4cfe64aacb627fd35765dcacd2c32393a7
anychhoice <contact@yechan.dev>
2022-02-24 16:30:20 +0900

    do not turn

--- double3/robot/base_robot.py
+++ double3/robot/base_robot.py
@@ -155,7 +155,8 @@ class CheckRobotThread(StoppableThread):
                 else:
                     clockwise = closest_bounding_box.left+closest_bounding_box.width/2-0.5
                     self.update_moving_strategies(
-                        [MovingStrategyDrive(0.5, clockwise)])
+                        # [MovingStrategyDrive(0.5, clockwise)])
+                        [MovingStrategyDrive(0.5, 0)])
             else:
                 self.update_moving_strategies([MovingStrategyStop()])
 
